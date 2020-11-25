d48db4075d9d2730b6252f01dcf240a31de10617
YOSHIHIRO YONEYAMA <twice.yoneyama@gmail.com>
2020-11-25 09:05:25 +0900

    test

--- src/main/java/com/example/demo/controller/MapController.java
+++ src/main/java/com/example/demo/controller/MapController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+//テスト
 
 @Controller
 public class MapController {
 	@RequestMapping("")
+	
 	public String mapDisp() {
 		return "homemap";
 	}
