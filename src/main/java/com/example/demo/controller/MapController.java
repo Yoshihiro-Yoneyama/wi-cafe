e3656eda98cdbe3673eb4b86a0e8ce91b0a2ea49
YOSHIHIRO YONEYAMA <twice.yoneyama@gmail.com>
2020-12-04 15:39:31 +0900

    Revert "完成"
    This reverts commit 9291640f1bd8f1f86ee9d6f9198cd665aa317087.

--- src/main/java/com/example/demo/controller/MapController.java
+++ src/main/java/com/example/demo/controller/MapController.java
@@ -1,24 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.demo.form.MapForm;
-import com.example.demo.form.User;
-import com.example.demo.service.MapService;
+//テスト
 
 @Controller
 public class MapController {
-	
-	@Autowired MapService mapService;
-	
 	@RequestMapping("")
-	public String mapDisp(Model model, User user, MapForm mapForm) {
-		String test = mapService.exJSON();
-		mapForm.setJsonRslt(test);
-		model.addAttribute("mapForm" ,mapForm);
+	public String mapDisp() {
 		return "homemap";
 	}
 	
